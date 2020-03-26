4a4ac5282f1642874d46b4e9d4b56379c7f9a43b
andreyt <fitality1@gmail.com>
2020-03-26 15:48:05 +0300

    first

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <button @click="generate" :disabled="loading">GENERATE DATA</button>
+        <p v-if="loading">{{counter}}/{{strCount / chankLength}}</p>
+        <hr>
+        <input type="text" v-model="input"/><button @click="find">FIND</button>
+        <p><span v-if="searchLoading">Loading...</span><span v-else>Result: {{result}}</span></p>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import {randString} from './helpers';
+    import localforage from 'localforage';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                loading: false,
+                searchLoading: false,
+                counter: 0,
+                input: '',
+                result: 0,
+                strCount: 10**7,
+                chankLength: 10**6
+            };
+        },
+        methods: {
+            async generate() {
+                console.log('START AT:'+ new Date());
+                this.loading = true;
+
+                await localforage.setDriver(localforage.INDEXEDDB);
+                await localforage.clear();
+
+                let str = '';
+
+                let j = 0;
+                for (let i = 0; i < this.strCount; i++) {
+                    str += randString(100) + ':';
+
+                    if (!(i % this.chankLength)) {
+                        await localforage.setItem(j.toString(), str);
+                        this.counter++;
+                        j++;
+                        str = '';
+                    }
+                }
+                this.loading = false;
+                console.log('END AT:'+ new Date());
+            },
+            async find() {
+                await localforage.setDriver(localforage.INDEXEDDB);
+
+                this.result = 0;
+                this.searchLoading = true;
+                console.log('START', new Date());
+                const reg = new RegExp(`(^|\\:)${this.input}`, 'g');
+                for (let i = 0; i < this.strCount / this.chankLength; i++){
+                    const str = await localforage.getItem(i.toString());
+                    // console.log(new Date(), i);
+                    const res = str.match(reg);
+                    console.log(`Strings [${i * this.chankLength} - ${(i+1) * this.chankLength}] match - ${res ? res.length : 0}|`, res);
+                    if(res) this.result += res.length;
+                }
+                console.log('END', new Date());
+                this.searchLoading = false;
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
